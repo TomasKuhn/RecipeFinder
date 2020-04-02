d764c9334d3a2258070f10600e83c2bf65b27630
Tomas Kuhn <kuhn23tomas@gmail.com>
2020-04-02 19:27:31 +0200

    Remove google comments

--- app/src/main/java/com/tkuhn/recipefinder/datasource/network/NetworkBoundResource.kt
+++ app/src/main/java/com/tkuhn/recipefinder/datasource/network/NetworkBoundResource.kt
@@ -1,43 +1,11 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.tkuhn.recipefinder.datasource.network
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.emitAll
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.mapNotNull
-import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.*
 import retrofit2.Response
 import timber.log.Timber
 
-/**
- * A generic class that can provide a resource backed by both the sqlite database and the network.
- *
- *
- * You can read more about it in the [Architecture
- * Guide](https://developer.android.com/arch).
- * @param <ResultType>
- * @param <ResponseType>
-</ResponseType></ResultType> */
 abstract class NetworkBoundResource<ResultType, ResponseType> {
 
     fun loadData(): Flow<Resource<ResultType>> {
